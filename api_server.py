d522d281715526a198094a5fba120860a02cd538
ashgit05 <97079936+ashgit05@users.noreply.github.com>
2026-05-29 11:13:28 +0530

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -329,4 +329,5 @@ def get_live_prices(req: PriceRequest):
     return {"prices": results}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
